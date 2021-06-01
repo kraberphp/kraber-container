530301e9c558b75fc91aac4f07375834ea395432
Florian Pisani <42639605+f-pisani@users.noreply.github.com>
2021-06-01 02:16:28 +0200

    Add missing psr4 autoload tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,5 @@ declare(strict_types=1);
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->addPsr4('Kraber\\Test\\', __DIR__ . "/");
 $loader->addPsr4('Kraber\\Test\\Unit\\', __DIR__ . "/unit");
+$loader->addPsr4('Kraber\\Test\\Unit\\Fixtures\\', __DIR__ . "/unit/fixtures");
 $loader->addPsr4('Kraber\\Test\\Integration\\', __DIR__ . "/integration");
